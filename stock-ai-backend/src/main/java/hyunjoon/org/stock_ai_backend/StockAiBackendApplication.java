c5981ebd84f1e73fc1cbd35ec5c689dc964a341e
hyeonjoonpark <pjjoon1379@gmail.com>
2026-04-21 19:19:07 +0900

    stock-ai backend setting

---
+++ stock-ai-backend/src/main/java/hyunjoon/org/stock_ai_backend/StockAiBackendApplication.java
@@ -0,0 +1,13 @@
+package hyunjoon.org.stock_ai_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockAiBackendApplication.class, args);
+	}
+
+}
---
+++ stock-ai-backend/src/test/java/hyunjoon/org/stock_ai_backend/StockAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hyunjoon.org.stock_ai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockAiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
